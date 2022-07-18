772e5d0b8e043a7ef0eea2e080839af4f120e7fe
Shruti Kothari <shrutikothari@shrutis-air.localdomain>
2022-07-18 23:14:16 +0200

    new branch added with order page files

---
+++ src/tests/OrderPageTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class OrderPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is test file");
+		
+	}
+
+}
