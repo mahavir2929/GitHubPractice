7a3993a90dd4703d991c1f43ccd83d1ed3619e37
mahavir29 <mahavir000@gmail.com>
2022-07-18 20:24:05 +0200

    commit on feature branch

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -3,8 +3,8 @@ package pages;
 public class HomePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("this is home page");
 	}
 
 }
---
+++ src/tests/SearchPageTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class SearchPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("search page test added");
+	}
+
+}
