cddc5f63ed50cc8811e993c74181b08fc293f4fb
mahavir29 <mahavir000@gmail.com>
2022-07-18 18:55:52 +0200

    third commit

--- src/tests/LoginTest.java
+++ src/tests/LoginTest.java
@@ -3,8 +3,8 @@ package tests;
 public class LoginTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("this is login test");
+		System.out.println("this is second line test");
 	}
 
 }
