a8375e7e31b0634be8b0a4ee901b71cd30fbd708
mahavir29 <mahavir000@gmail.com>
2022-07-19 19:45:59 +0200

    commit registration page

---
+++ src/pages/RegistrationPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class RegistrationPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("this is registration page");
+	}
+
+}
